ff404a3c0d140a027eab3d2817b5f1602a972263
Che4891 <antoon.izotov@yandex.ru>
2021-08-03 06:57:59 +0300

    add js if or else and for

--- first/js/main.js
+++ first/js/main.js
@@ -78,25 +78,102 @@ let incr = 10,
 
 	//ПРАКТИЧЕСКОЕ ЗАДАНИЕ
 
-	const numderOfFilms = +prompt('Как много фильмов вы посмотрели?, "');
-	console.log(numderOfFilms);
+	// const numderOfFilms = +prompt('Как много фильмов вы посмотрели?, "');
+	// console.log(numderOfFilms);
 
-	const personalMovieDB = {
-		count: numderOfFilms,
-		movies: {},
-		actors: {},
-		genres: [],
-		privat: false
-	}; 
+	// const personalMovieDB = {
+	// 	count: numderOfFilms,
+	// 	movies: {},
+	// 	actors: {},
+	// 	genres: [],
+	// 	privat: false
+	// }; 
 
 
-	const a = prompt('Один из последних просмотренніх фильмов?');
-	const b = prompt('На сколько вы их оцените?');
+	// const a = prompt('Один из последних просмотренніх фильмов?');
+	// const b = prompt('На сколько вы их оцените?');
 
-	const d = prompt('Один из последних просмотренніх фильмов?');
-	const c = prompt('На сколько вы их оцените?');
+	// const d = prompt('Один из последних просмотренніх фильмов?');
+	// const c = prompt('На сколько вы их оцените?');
 	
-	personalMovieDB.movies[a] = b; 
-	personalMovieDB.movies[d] = c; 
+	// personalMovieDB.movies[a] = b; 
+	// personalMovieDB.movies[d] = c; 
 
-	console.log(personalMovieDB);
+	// console.log(personalMovieDB);
+
+	//Условия IF & ELSE
+	const testing = 1;
+
+	if (testing) {
+		console.log('Ok');
+	} else {
+		console.log('not ok');
+	}
+
+	// много уровневая проверка
+
+	const numbers = 50;
+
+	if (numbers < 49) {
+		console.log('first');
+	} else if ( numbers > 100 ) {
+		console.log('second');
+	} else {
+		console.log('three');
+	}
+	// сравнение тернарными операторами
+
+	const num = 50;
+
+	(num == 50) ? console.log('Okc') : console.log("Not Ok");
+
+	//многоуровневая проверка SWITCH
+
+	const nures = '50';
+
+	switch (nures) {
+		case '49':
+			console.log('not');
+			break;
+		case '150':
+			console.log('not');
+			break;
+		case '50':
+			console.log('yes');
+			break;
+		default:
+			console.log('не в этот раз');
+			break;
+	}
+
+	// ЦЫКЛЫ
+// while
+	let chukl = 50;
+
+	while (chukl <= 55) {
+		console.log(chukl);
+		chukl++;
+	}
+//do
+
+	let chukl1 = 50;
+
+	do {
+		console.log(chukl1);
+		chukl1++;
+	}
+	while (chukl1 <= 55);
+//for 
+let nurt = 50;
+for (let i = 1; i < 8; i++) {
+	console.log(nurt);
+	nurt++;
+}
+
+for (let i = 1; i < 10; i++) {
+	if (i===6) {
+		//break; - заканчивает цикл когда выполняеться условие
+		continue; // выполняет цыкл до условия и после (перепрыгивает)
+	}
+	console.log(i);
+}
