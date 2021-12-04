f75fa05a6b2520b113f123ee9da2bc88613897eb
Che4891 <antoon.izotov@yandex.ru>
2021-12-04 08:14:34 +0200

    added modal scripts

--- mobail/js/mobile.js
+++ mobail/js/mobile.js
@@ -1,1 +1,16 @@
-console.log('Hello world');
+window.addEventListener('DOMContentLoaded', () => {
+    const box = document.querySelector('.box');
+
+    box.addEventListener('touchstart', (e) => {
+        e.preventDefault();
+        console.log('Start');
+    });
+    box.addEventListener('touchmove', (e) => {
+        e.preventDefault();
+        console.log('Move');
+    });
+    box.addEventListener('touchend', (e) => {
+        e.preventDefault();
+        console.log('End');
+    });
+})
